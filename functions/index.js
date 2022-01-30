7695cc52f39f74eb2f0f48e57dae928e190ad6d8
nico-galin <nico.galin@gmail.com>
2022-01-30 14:25:43 -0800

    Fixed dateString issues

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,11 @@ exports.generateWords = functions.pubsub.schedule('0 0 * * *')
     .timeZone('America/Los_Angeles') // Users can choose timezone - default is America/Los_Angeles
     .onRun(async (context) => {
     admin.initializeApp();
-    const today = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"})
-    const dd = String(today.getDate()).padStart(2, '0');
-    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    const yyyy = today.getFullYear();
+    const dateString = new Date().toLocaleString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        timeZone: "America/Los_Angeles"}).replaceAll("/", "");
     Promise.all([
         fetch("https://api.datamuse.com/words?sp=????"),
         fetch("https://api.datamuse.com/words?sp=?????"),
@@ -18,10 +19,10 @@ exports.generateWords = functions.pubsub.schedule('0 0 * * *')
             4: four[Math.floor(Math.random()*four.length)]["word"],
             5: five[Math.floor(Math.random()*five.length)]["word"],
             6: six[Math.floor(Math.random()*six.length)]["word"],
-            "date": `${mm}${dd}${yyyy}`
+            "date": dateString
         }).then(writeResult => {
             // write is complete here
-            console.log( `[${mm}${dd}${yyyy}] Added words`)
+            // console.log( `[${dateString}] Added words`)
         });
     })
 });
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -91,11 +91,12 @@ const signOut = () => {
 
 const getTodaysWords = async () => {
     try {
-        const today = new Date();
-        const dd = String(today.getDate()).padStart(2, '0');
-        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        const yyyy = today.getFullYear();
-        const q = query(collection(db, "words"), where("date", "==", `${mm}${dd}${yyyy}`));
+        const dateString = new Date().toLocaleString("en-US", {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            timeZone: "America/Los_Angeles"}).replaceAll("/", "");
+        const q = query(collection(db, "words"), where("date", "==", `${dateString}`));
         const docs = await getDocs(q);
         const data = [];
         docs.forEach(doc => {
